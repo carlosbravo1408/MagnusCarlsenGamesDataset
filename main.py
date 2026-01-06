e39759e2e9061374308236b34dd9ac45ac266205
carlos <carlos.j.bravo@hotmail.com>
2026-01-06 18:53:05 -0500

    feat: Add main.py entry point and request.txt

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.api_helper import APIHelper
+
+
+if __name__ == "__main__":
+    api = APIHelper()
+    api.get_chess_games(user_name="MagnusCarlsen")
+    api.get_chess_games(user_name="DrNykterstein")
